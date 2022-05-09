5868e15d9de76142ea9a842b191e951de7ccbd84
Debleena18 <sarkardebleena18@gmail.com>
2022-05-09 11:32:44 +0530

    applying general layout universally through _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,13 @@
+//To apply universal layout we wrap the _app.js with layout.
+//With this universal layout happens
+
+import Layout from '../components/layout/Layout'
 import '../styles/globals.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return <Layout>
+    <Component {...pageProps} />
+    </Layout>
 }
 
 export default MyApp
